6138682d9b517671298fdc42b7ad5d6e3485c46c
Luke Ramsay <lukram98@gmail.com>
2018-07-08 13:26:19 -0400

    First commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD_VDW1uMGzNoVmfTXtlUjGP-lWMDNPLFE",
+    authDomain: "rps-multiplayer-ac314.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-ac314.firebaseio.com",
+    projectId: "rps-multiplayer-ac314",
+    storageBucket: "",
+    messagingSenderId: "982260294819"
+  };
+  firebase.initializeApp(config);
