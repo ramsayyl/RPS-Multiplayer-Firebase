bc6abf16ede59dee1131589150f5a99327774616
Luke Ramsay <lukram98@gmail.com>
2018-07-10 16:46:55 -0400

    Final Changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,10 +13,6 @@ var messageRef = database.ref('messages');
 var playerRef = database.ref('player');
 var opponentRef = database.ref('opponent');
 
-// var $time = "";
-var pChoice;
-var oChoice;
-
 var $player = {
         number: '0',
         name: '',
@@ -39,13 +35,11 @@ var $player = {
 
 $("#player-submit-button").on("click", function(event) {
   event.preventDefault();
-
   $player.message = $("#player-message-field").val().trim();
 
     firebase.database().ref('messages').set({
       name: $player.name,
       message: $player.message,
-      // time: Date.now()
     });
 
   $("#player-message-field").val("");
@@ -53,13 +47,11 @@ $("#player-submit-button").on("click", function(event) {
 
 $("#opponent-submit-button").on("click", function(event) {
   event.preventDefault();
-
   $opponent.message = $("#opponent-message-field").val().trim();
 
     firebase.database().ref('messages').set({
       name: $opponent.name,
       message: $opponent.message,
-      // time: Date.now()
     });
 
   $("#opponent-message-field").val("");
@@ -96,7 +88,7 @@ $("#submit-opponent-username").on("click", function(event) {
     $(".opponent-user-form").hide();
 
     $(".opponent-panel-heading").text($opponent.name)
-  $("#opponent-username").val("");
+    $("#opponent-username").val("");
 });
 
 firebase.database().ref('player').on("value", function(snapshot){
@@ -106,17 +98,14 @@ firebase.database().ref('player').on("value", function(snapshot){
 
   winCheck();
 
-
 });
 
 firebase.database().ref('opponent').on("value", function(snapshot){
   $opponent.choice = snapshot.val().choice;
-
   $(".opponent-panel-heading").text(snapshot.val().name);
   $(".opponent-win-loss").text("Wins: " + snapshot.val().wins + " | " + "Losses: " + snapshot.val().losses);
 
   winCheck();
-
 
 });
 
@@ -125,14 +114,11 @@ $(".player-rock").on("click", function(event){
 
   if ($player.name.length > 0 && $opponent.name.length > 0) {
     $player.choice = $(".player-rock").attr("data-text");
-    // console.log($player.choice);
 
     firebase.database().ref('player').set({
       choice: $player.choice
     });
-
   }
-
 });
 
 $(".player-paper").on("click", function(event){
@@ -140,14 +126,11 @@ $(".player-paper").on("click", function(event){
 
   if ($player.name.length > 0 && $opponent.name.length > 0) {
     $player.choice = $(".player-paper").attr("data-text");
-    // console.log($player.choice);
 
     firebase.database().ref('player').set({
       choice: $player.choice
     });
-
   }
-
 });
 
 $(".player-scissors").on("click", function(event){
@@ -155,14 +138,11 @@ $(".player-scissors").on("click", function(event){
 
   if ($player.name.length > 0 && $opponent.name.length > 0) {
     $player.choice = $(".player-scissors").attr("data-text");
-    // console.log($player.choice);
 
     firebase.database().ref('player').set({
       choice: $player.choice
     });
-
   }
-
 });
 
 $(".opponent-rock").on("click", function(event) {
@@ -170,14 +150,11 @@ $(".opponent-rock").on("click", function(event) {
 
   if ($player.name.length > 0 && $opponent.name.length > 0) {
     $opponent.choice = $(".opponent-rock").attr("data-text");
-    // console.log($opponent.choice);
 
     firebase.database().ref('opponent').set({
       choice: $opponent.choice
     });
-
   }
-
 });
 
 $(".opponent-paper").on("click", function(event) {
@@ -185,14 +162,11 @@ $(".opponent-paper").on("click", function(event) {
 
   if ($player.name.length > 0 && $opponent.name.length > 0) {
     $opponent.choice = $(".opponent-paper").attr("data-text");
-    // console.log($opponent.choice);
 
     firebase.database().ref('opponent').set({
       choice: $opponent.choice
     });
-
   }
-
 });
 
 $(".opponent-scissors").on("click", function(event) {
@@ -200,21 +174,16 @@ $(".opponent-scissors").on("click", function(event) {
 
   if ($player.name.length > 0 && $opponent.name.length > 0) {
     $opponent.choice = $(".opponent-scissors").attr("data-text");
-    // console.log($opponent.choice);
 
     firebase.database().ref('opponent').set({
       choice: $opponent.choice
     });
-
   }
-
 });
 
 function winCheck() {
 
-  // console.log($player.choice + " " + $opponent.choice)
-
-  if ($player.choice.length > 0 && $opponent.choice.length > 0) {
+  if ($player.choice !== null && $opponent.choice !== null) {
     if ($player.choice.length === $opponent.choice.length){
       $("#results-area").text("Tie!");
     }
@@ -257,21 +226,13 @@ function winCheck() {
     $player.choice = "";
     $opponent.choice = "";
   }
+  firebase.database().ref('player').set({
+    wins: $player.wins,
+    losses: $player.losses
+  });
 
-
-}
-
-firebase.database().ref('player').set({
-  wins: $player.wins,
-  losses: $player.losses
-});
-firebase.database().ref('opponent').set({
-  wins: $opponent.wins,
-  losses: $opponent.losses
-});
-
-if (($player.choice === null & $opponent.choice > 0) || (($opponent.choice === null & $player.choice > 0))) {
-  $player.wins = $player.wins;
-  $opponent.wins = $opponent.wins;
-
+  firebase.database().ref('opponent').set({
+    wins: $opponent.wins,
+    losses: $opponent.losses
+  });
 }
